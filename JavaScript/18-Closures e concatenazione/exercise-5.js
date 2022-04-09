81d7000f4a572648e18e3148f50fea6808cb86a6
Aurelio Z <whytemagic@gmail.com>
2022-04-09 16:23:00 +0200

    risolto esercizio 18

--- JavaScript/18-Closures e concatenazione/exercise-5.js
+++ JavaScript/18-Closures e concatenazione/exercise-5.js
@@ -1,25 +1,28 @@
 function memoize(fn) {
   let cache = {};
-  if(fn in cache){
-    return 'Fetching from cache for' + fn
-  }else{
-    //cache = new Object({x: fn});
-    //console.log(fn)
-    //console.log(cache)
-    return 'Calculating result for' + (fn)
+  return function (x){
+      if(x in cache){
+        return 'Fetching from cache' + ' ' + fn(x)
+      }else{
+        //resultfactorial = fn(x)
+        cache[x] = fn(x)
+        //console.log(cache)
+        return 'Calculating result for' +' ' + fn(x)
+      }
   }
 }
 
-function factorial(x) {
+function calcfactorial(x) {
   if (x === 0) {
     return 1;
   }
-  return x * factorial(x - 1);
+  return x * calcfactorial(x - 1);
 }
 
-let calcfactorial = memoize(factorial);
+factorial = memoize(calcfactorial);
 console.log(factorial(10));
 console.log(factorial(6));
 console.log(factorial(5));
 console.log(factorial(5));
 console.log(factorial(0));
+console.log(factorial(6));

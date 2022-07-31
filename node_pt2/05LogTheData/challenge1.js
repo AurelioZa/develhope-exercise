6b15b9fdac2e0a75abcd5fafb4a88bed4bc3f15f
Aurelio Z <whytemagic@gmail.com>
2022-07-31 11:26:12 +0200

    Node.js Pt.2 - Esercizi 3-4-5-6 aggiunto gitignore

---
+++ node_pt2/05LogTheData/challenge1.js
@@ -0,0 +1,10 @@
+// Challenge 1:
+//
+// Use 2 different techniques to output the value of this variable with
+// a label, so we can easily identify it in the script output.
+
+const surprisingFact = "The bumblebee bat is the world's smallest mammal";
+
+console.log(`La prima label è: ${surprisingFact}` );
+
+console.log("La seconda label è:", surprisingFact);
